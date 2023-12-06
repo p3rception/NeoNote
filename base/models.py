5b8fd3e365ffd79e51394657dfcaaed554f14330
dimitris-per <dim.pergel@gmail.com>
2023-12-06 03:53:21 +0200

    Implemented User-specific data access

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.http import request
 
 # Django Models = Classes that represent Database tables
 
@@ -9,7 +10,7 @@ class Task(models.Model):
    description = models.TextField(null=True, blank=True)
    complete = models.BooleanField(default=False)
    created = models.DateTimeField(auto_now_add=True)
-
+   
    def __str__(self):
       return self.title
 
