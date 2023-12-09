4817718cb3fb27e9355b878b116521bc2c2de0ba
dimitris-per <dim.pergel@gmail.com>
2023-12-09 16:08:57 +0200

    Disabled password validation for testing

--- NeoNote/settings.py
+++ NeoNote/settings.py
@@ -85,6 +85,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
+
+# Disabled for testing purposes while in development
 AUTH_PASSWORD_VALIDATORS = [
     # {
     #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
